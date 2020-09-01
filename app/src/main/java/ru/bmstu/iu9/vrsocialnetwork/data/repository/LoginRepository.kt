6471b3d13f3885f640bf6df4f7ecff4be4c144ad
Andrey <and.vladislavov@gmail.com>
2020-09-01 13:48:26 +0300

    sub to topic

--- app/src/main/java/ru/bmstu/iu9/vrsocialnetwork/data/repository/LoginRepository.kt
+++ app/src/main/java/ru/bmstu/iu9/vrsocialnetwork/data/repository/LoginRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.coroutines.tasks.await
 import ru.bmstu.iu9.vrsocialnetwork.data.model.User
 import javax.inject.Inject
@@ -16,50 +17,50 @@ import javax.inject.Inject
  */
 
 class LoginRepository @Inject constructor(private val mMainRepository: MainRepository) {
-    private val mAuth = FirebaseAuth.getInstance()
+	private val mAuth = FirebaseAuth.getInstance()
 
-    fun logOut() {
-        mAuth.signOut()
-    }
+	fun logOut() {
+		mAuth.signOut()
+        FirebaseMessaging.getInstance().unsubscribeFromTopic(mAuth.currentUser!!.uid)
+	}
 
-    suspend fun firebaseSignInWithGoogle(googleAuthCredential: AuthCredential): User {
-        var authenticatedUser = User()
-        // TODO run in coroutine
-        val task = mAuth.signInWithCredential(googleAuthCredential).await()
-            if (task != null) {
-                val isNewUser: Boolean =
-                    task.additionalUserInfo!!.isNewUser
-                val firebaseUser: FirebaseUser? = mAuth.currentUser
-                if (firebaseUser != null) {
-                    val uid = firebaseUser.uid
-                    val name = firebaseUser.displayName?: "No name"
-                    val email = firebaseUser.email?: "No email"
-                    val user = User(
-                            uId = uid,
-                            name = name,
-                            email = email
-                        )
-                    Log.d(TAG, user.toString())
-                    mMainRepository.addClient(user)
-                    user.isNew = isNewUser
-                    authenticatedUser = user
-                }
-            } else {
-                Log.e(TAG, "Not successful")
-            }
+	suspend fun firebaseSignInWithGoogle(googleAuthCredential: AuthCredential): User {
+		var authenticatedUser = User()
+		val task = mAuth.signInWithCredential(googleAuthCredential).await()
+		if (task != null) {
+			val isNewUser: Boolean =
+				task.additionalUserInfo!!.isNewUser
+			val firebaseUser: FirebaseUser? = mAuth.currentUser
+			if (firebaseUser != null) {
+				val uid = firebaseUser.uid
+				val name = firebaseUser.displayName ?: "No name"
+				val email = firebaseUser.email ?: "No email"
+				val user = User(
+                    uId = uid,
+                    name = name,
+                    email = email
+                )
+				Log.d(TAG, user.toString())
+				mMainRepository.addClient(user)
+				user.isNew = isNewUser
+				authenticatedUser = user
+			}
+            FirebaseMessaging.getInstance().subscribeToTopic(mAuth.currentUser!!.uid)
+		} else {
+			Log.e(TAG, "Not successful")
+		}
 
-        return authenticatedUser
-    }
+		return authenticatedUser
+	}
 
-    fun createNewUserIfNorExist(user: User): MutableLiveData<User> {
-        val newUserLiveData = MutableLiveData<User>()
-        // TODO add to firebase (or send to server so it will create)
-        user.isCreated = true
-        newUserLiveData.value = user
-        return newUserLiveData
-    }
+	fun createNewUserIfNorExist(user: User): MutableLiveData<User> {
+		val newUserLiveData = MutableLiveData<User>()
+		user.isCreated = true
+		newUserLiveData.value = user
+		return newUserLiveData
+	}
 
-    companion object {
-        private const val TAG = "LOGIN REP"
-    }
+	companion object {
+		private const val TAG = "LOGIN REP"
+	}
 }
